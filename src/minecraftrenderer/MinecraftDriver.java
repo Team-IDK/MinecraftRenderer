9ad1b374208b417a10d93a0afbb1e0ad7343ddc1
Kyle Hubbard <kyle.leehubbard@gmail.com>
2019-03-12 11:55:05 -0700

    Project Packages and ZBuffering
    Moved the project to use packages to better facilitate working between PCs.
    
    Re added the project.properties file, removed from .gitignore.
    
    Added Z-Buffering for primitives so that they draw correctly.

---
+++ src/minecraftrenderer/MinecraftDriver.java
@@ -0,0 +1,75 @@
+/***************************************************************
+* file: MinecraftDriver.java
+* author: Team NULL - Kyle Hubbard
+* class: CS 4450 - Computer Graphics
+*
+* assignment: final program
+* date last modified: 3/5/2019
+*
+* purpose: This program is the main class for the MincraftRenderer and it
+* acts as the entry point for the entire rendering process.
+*
+****************************************************************/
+
+package minecraftrenderer;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.util.glu.GLU;
+
+public class MinecraftDriver {
+    
+    private FPCameraController fp = new FPCameraController(0, 0, 0);
+    private DisplayMode displayMode;
+    
+    // method: start
+    // purpose: this method calls all of the necessary rendering functions
+    public void start() {
+        
+        try {
+            createWindow();
+            initGL();
+            fp.gameLoop();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    // method: createWindow
+    // purpose: this method creates the display stage and assigns the display mode
+    private void createWindow() throws Exception {
+        
+        Display.setFullscreen(false);
+        DisplayMode d[] = Display.getAvailableDisplayModes();
+        for(int i = 0; i < d.length; i++) {
+            if(d[i].getWidth() == 640 && d[i].getHeight() == 480 && d[i].getBitsPerPixel() == 32) {
+                displayMode = d[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
+        Display.setTitle("Team NULL - Minecraft Renderer");
+        Display.create();
+    }
+
+    // method: initGL
+    // purpose: this method initializes the matrix mode, matrix model, and perspective attributes
+    private void initGL() {
+        
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        GLU.gluPerspective(100.0f, (float)displayMode.getWidth() / (float)displayMode.getHeight(), 0.1f, 300.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    }
+    
+    // method: main
+    // purpose: this method creates a new MinecraftDriver object and calls its start method
+    public static void main(String[] args) {
+
+        MinecraftDriver renderer = new MinecraftDriver();
+        renderer.start();
+    }
+}
